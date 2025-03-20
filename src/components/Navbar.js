379c830ccdb57413750fae184cafd7ed75f0b13f
Richyb78 <richyb78@googlemail.com>
2025-03-20 15:33:13 +0000

    Further development of contact page and overall responsiveness of site.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "../styles/Navbar.css";
 import Logo from "../assets/Lsclogo.png";
+import LogoSmall from "../assets/lsclogosmall.png";
 import { Link } from "react-router-dom";
 import ReorderIcon from '@mui/icons-material/Reorder';
 
@@ -10,13 +11,13 @@ function Navbar() {
     setOpenLinks(!openLinks);
   }
 
-
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="love south chingford logo" />
         <h2>Love South Chingford</h2>
         <div className="hiddenLinks">
+            <img src={LogoSmall} alt="small love south chingford logo" className="logoSmall"/>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/events">Events</Link>
@@ -32,7 +33,6 @@ function Navbar() {
         <ReorderIcon />
         </button>
       </div>
-      
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "../styles/About.css";
 import AboutPic from "../assets/aboutpic.jpg"
+import LogoSmall from "../assets/lsclogosmall.png";
 
 function About() {
   return (
     <div className="about">
       <div className="aboutTop" style={{backgroundImage: `url(${AboutPic})`}}></div>
       <div className="aboutBottom">
+      <img src={LogoSmall} alt="small love south chingford logo" className="logoSmall"/>
       <h2>About Us</h2>
       <h3>
           Love South Chingford E4 (LSCE4) is a not-for-profit group of volunteers interested
@@ -30,6 +32,7 @@ function About() {
           <li>Help restore Memorial Park pond.</li>
           <li>Help secure renovation to Memorial Park main entrance (ongoing).</li>
           <li>Helped promote PSPO (Public Space Protection Order).</li>
+          <li>Organised for two emergency defibrillator machines to be installed at Chingford Mount.</li>
           <li>Secured a Christmas tree to be proud of on Albert Crescent.</li>
           <li>Organised the Christmas Switch-on event on Albert Crescent</li>
           </ul>
