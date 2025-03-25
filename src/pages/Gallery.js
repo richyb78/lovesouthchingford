faffdb6093907443bb05d2e69482b9d992ec79ce
Richyb78 <richyb78@googlemail.com>
2025-03-25 15:43:37 +0000

    Tidyied up the css files

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -9,24 +9,31 @@ function Gallery() {
   return (
     <>
     <div>
-      <h3 className="galleryTitle">Gardening Around South Chingford</h3>
-      <p className="galleryblurb"> We engage in regular and varied gardening activities
-                                   around Chingford Cemetery, Memorial Park and the Crescent to promote
-                                   environmental awareness, social connections and contribute to a greener community for all.
-                                   </p>
+        <h3 className="galleryTitle">Gardening Around South Chingford</h3>
+          <p className="galleryblurb"> 
+              We engage in regular and varied gardening activities
+              around Chingford Cemetery, Memorial Park and the Crescent to promote
+              environmental awareness, social connections and contribute to a greener community for all.
+          </p>
       <GardeningGallery />
       <h3 className="galleryTitle">Easter at Chingford Mount</h3>
-      <p className="galleryblurb">We have organised Easter markets situated around Albert Crescent where the community can join in festive activities celebrating spring.
-                                   </p>
+      <p className="galleryblurb">
+          We have organised Easter markets situated around Albert Crescent where the 
+          community can join in festive activities celebrating spring.
+      </p>
       <EasterGallery />
       <h3 className="galleryTitle">Christmas Tree Switch on Event</h3>
-      <p className="galleryblurb">Our Christmas switch on event was a great success and included many stalls, a father Christmas and real snow!
-                                  </p>
+      <p className="galleryblurb">
+          Our Christmas switch on event was a great success and included
+          many stalls, a father Christmas and real snow!
+      </p>
       <XmasGallery />
       <h3 className="galleryTitle">The Repair Club</h3>
-      <p className="galleryblurb">We run regular repair clubs in 228 Chingford Mount for the public to bring in broken household items to be fixed by our expert fixers.</p>
+      <p className="galleryblurb">
+          We run regular repair clubs in 228 Chingford Mount for the public to 
+          bring in broken household items to be fixed by our expert fixers.
+      </p>
       <RepairClubGallery />
-
     </div>
     </>
   )
